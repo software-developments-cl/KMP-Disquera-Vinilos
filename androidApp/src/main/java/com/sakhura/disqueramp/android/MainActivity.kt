220d8bb8b1f038a6145c7c773143bb4a93f23faf
Anbs12 <118308826+Anbs12@users.noreply.github.com>
2025-07-08 21:10:05 -0400

    Integrate PreviewNecesaria into MainActivity and add click functionality
    This commit introduces the `PreviewNecesaria` composable into the `MainActivity`.
    The `GreetingView` in `MainActivity.kt` now:
    - Arranges its content in a centered column.
    - Displays the `PreviewNecesaria` composable.
    - Passes a click listener to `PreviewNecesaria` that shows a Toast message.
    
    The `PreviewNecesaria` composable in `NavAppMain.kt` now:
    - Accepts an `onClick` lambda function.
    - Becomes clickable, invoking the passed `onClick` function when tapped.
    
    The preview in `PreviewsFile.kt` has been commented out as it's now tested within `MainActivity`.

--- androidApp/src/main/java/com/sakhura/disqueramp/android/MainActivity.kt
+++ androidApp/src/main/java/com/sakhura/disqueramp/android/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.sakhura.disqueramp.android
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sakhura.disqueramp.Greeting
+import com.sakhura.disqueramp.navigation.PreviewNecesaria
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+
+                    GreetingView(Greeting().greet(), this)
+
                 }
             }
         }
@@ -27,14 +35,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView(text: String, context: Context) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        PreviewNecesaria(
+            { Toast.makeText(context, "Al fin funciono", Toast.LENGTH_SHORT).show() }
+        )
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        //GreetingView(Greeting().greet())
     }
 }
